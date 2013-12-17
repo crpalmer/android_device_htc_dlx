86a34f0a68214dd9fb8101ddf87b4745ab1eaf14
Christopher R. Palmer <crpalmer@gmail.com>
2013-12-17 05:34:27 -0500

    dlx: Add my own (CRPALMER) build id
    Change-Id: I197e021c44e96507b602b3973a5342e53fc5430c

--- cm.mk
+++ cm.mk
@@ -5,6 +5,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Release name
 PRODUCT_RELEASE_NAME := dlx
 
+# crpalmer specific
+TARGET_UNOFFICIAL_BUILD_ID := crpalmer
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
